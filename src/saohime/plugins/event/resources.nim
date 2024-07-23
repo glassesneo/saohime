30a68083fa80254685c4cfe75dcdd522227008e4
glassesneo <glassesneo@protonmail.com>
2024-07-23 23:53:11 +0900

    :recycle: chore: split the module

---
+++ src/saohime/plugins/event/resources.nim
@@ -0,0 +1,31 @@
+{.push raises: [].}
+
+import
+  pkg/[ecslib, oolib, sdl2],
+  ../../core/[contract]
+
+class pub EventListener:
+  var event: Event
+
+  proc pollEvent*: bool =
+    return sdl2.pollEvent(self.event)
+
+  proc checkEvent*(eventType: EventType): bool =
+    return self.event.kind == eventType
+
+  proc checkQuitEvent*: bool =
+    return self.checkEvent(QuitEvent)
+
+  proc currentKey*: cint =
+    pre(self.checkEvent(KeyDown))
+
+    return self.event.key.keysym.sym
+
+  proc currentKeyName*: cstring =
+    return self.currentKey.getKeyName()
+
+  proc currentButton*: uint8 =
+    return self.event.button.button
+
+export new
+
