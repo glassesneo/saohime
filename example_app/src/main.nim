fba05ac9c8f66e5a6a9c3a5a51a2d450a44546f6
glassesneo <glassesneo@protonmail.com>
2024-08-15 21:58:43 +0900

    :white_check_mark: example: update example app

--- example_app/src/main.nim
+++ example_app/src/main.nim
@@ -1,6 +1,10 @@
 import
+  std/[math],
   ../../src/saohime,
   ../../src/saohime/default_plugins
+
+type
+  Player = ref object
 
 proc pollEvent(appEvent: Event[ApplicationEvent]) {.system.} =
   for e in appEvent:
@@ -9,21 +13,51 @@ proc pollEvent(appEvent: Event[ApplicationEvent]) {.system.} =
 
 let app = Application.new()
 
-# proc load(assetManager: Resource[AssetManager]) {.system.} =
-#   let texture = assetManager.loadImage("cat.jpg")
-#
-#   let cat = commands.create()
-#     .attach(texture)
-#     .attach(Transform.new(
-#       x = 0, y = 0,
-#       scale = Vector.new(-0.2, -0.2),
-#       rotation = 0.5
-#     ))
+proc load(assetManager: Resource[AssetManager]) {.system.} =
+  let spriteSheet = assetManager.loadSpriteSheet(
+    "knight.png",
+    columnLen = 8,
+    rowLen = 8
+  )
+
+  let sprite = spriteSheet[0, 4]
+
+  commands.create()
+    .attach(Player())
+    .attach(sprite)
+    .attach(Transform.new(
+      x = 50, y = 300,
+      scale = Vector.new(3f, 3f)
+    ))
+
+proc playerMove(
+    All: [Player, Sprite, Transform],
+    fpsManager: Resource[FPSManager],
+    keyboardEvent: Event[KeyboardEvent]
+) {.system.} =
+  for e in keyboardEvent:
+    for sprite, transform in each(entities, [Sprite, Transform]):
+      if e.isDown(K_d):
+        transform.position.x += 5
+        transform.scale.x = transform.scale.x.abs
+        if fpsManager.frameCount mod 3 == 0:
+          sprite.rotateIndex()
+      elif e.isDown(K_a):
+        transform.position.x -= 5
+        transform.scale.x = -transform.scale.x.abs
+        if fpsManager.frameCount mod 3 == 0:
+          sprite.rotateIndex()
+      else:
+        sprite.currentIndex = 0
+
+
+  keyboardEvent.clearQueue()
+
 
 app.loadPluginGroup(DefaultPlugins)
 
 
 app.start:
-  world.registerSystems(pollEvent)
-  # world.registerStartupSystems(load)
+  world.registerSystems(pollEvent, playerMove)
+  world.registerStartupSystems(load)
 
